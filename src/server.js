d0cb03a373227bdc3e616ebc86677e55ac3c82d6
MashenP <mashenp@yahoo.com>
2026-07-01 15:43:27 +0200

    added nodejs-hw#1

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ app.get('/notes/:noteId', (req, res) => {
   const { noteId } = req.params;
 
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-  });
+});
 
 app.get('/test-error', (req, res) => {
   throw new Error('Simulated server error');
