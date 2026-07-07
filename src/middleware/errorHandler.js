df9f884d4bb483b251c2f52c2c0dcb1a25f8238b
MashenP <mashenp@yahoo.com>
2026-07-07 04:26:59 +0200

    nodejs-hw #2 is ready

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,17 @@
+import { isHttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  const isProd = process.env.NODE_ENV === "production";
+
+  if (isHttpError(err)) {
+    return res.status(err.status).json({
+      message: err.message || err.name,
+    });
+  };
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.stack,
+  });
+};
