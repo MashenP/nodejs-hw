6384eb5a993707483a5f2b0aaebbd922ea45a4fa
MashenP <mashenp@yahoo.com>
2026-07-11 15:21:21 +0200

    nodejs-hw#3 is ready

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -2,9 +2,38 @@ import createHttpError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
-  const notes = await Note.find();
+  const { page = 1, perPage = 10, tag, search } = req.query;
+  const skip = (page - 1) * perPage;
 
-  res.status(200).json(notes);
+  const noteQuery = Note.find();
+
+  if (search) {
+    noteQuery.where({
+      $or: [
+        { title: { $regex: search, $options: 'i' } },
+        { content: { $regex: search, $options: 'i'} },
+      ],
+    });
+  };
+
+  if (tag) {
+    noteQuery.where('tag').equals(tag);
+  };
+
+  const [totalNotes, notes] = await Promise.all([
+    noteQuery.clone().countDocuments(),
+    noteQuery.skip(skip).limit(perPage),
+  ]);
+
+  const totalPages = Math.ceil(totalNotes / perPage);
+
+  res.status(200).json({
+    page,
+    perPage,
+    totalNotes,
+    totalPages,
+    notes,
+  });
 };
 
 export const getNoteById = async (req, res) => {
